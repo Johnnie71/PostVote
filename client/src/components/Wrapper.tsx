99eef11fab03e1808c59f558c6d77949def0a316
johnnie71 <johnnie.agonz71@gmail.com>
2022-01-07 16:34:32 -0500

    added variant for different wrapper widths

--- client/src/components/Wrapper.tsx
+++ client/src/components/Wrapper.tsx
@@ -1,11 +1,21 @@
 import { Box } from "@chakra-ui/react";
 import React from "react";
 
-interface WrapperProps {}
+interface WrapperProps {
+	variant?: "small" | "regular";
+}
 
-export const Wrapper: React.FC<WrapperProps> = ({ children }) => {
+export const Wrapper: React.FC<WrapperProps> = ({
+	children,
+	variant = "regular",
+}) => {
 	return (
-		<Box mt={8} mx="auto" maxWidth="800px" w="100%">
+		<Box
+			mt={8}
+			mx="auto"
+			maxWidth={variant === "regular" ? "800px" : "400px"}
+			w="100%"
+		>
 			{children}
 		</Box>
 	);
