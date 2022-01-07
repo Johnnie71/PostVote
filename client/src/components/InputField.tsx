ba9eaaa56132c13b02301e26df412262da3c473a
johnnie71 <johnnie.agonz71@gmail.com>
2022-01-07 17:03:49 -0500

    input field

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -9,16 +9,19 @@ import React, { InputHTMLAttributes } from "react";
 
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
 	label: string;
-	placeholder: string;
 	name: string;
 };
 
-export const InputField: React.FC<InputFieldProps> = (props) => {
+export const InputField: React.FC<InputFieldProps> = ({
+	label,
+	size: _,
+	...props
+}) => {
 	const [field, { error }] = useField(props);
 	return (
 		<FormControl isInvalid={!!error}>
-			<FormLabel htmlFor={field.name}>{props.label}</FormLabel>
-			<Input {...field} id={field.name} placeholder={props.placeholder}></Input>
+			<FormLabel htmlFor={field.name}>{label}</FormLabel>
+			<Input {...field} {...props} id={field.name}></Input>
 			{error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
 		</FormControl>
 	);
