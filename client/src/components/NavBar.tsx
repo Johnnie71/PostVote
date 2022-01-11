0867e3d358b048bd973753de8cb3f95a3bb53a98
johnnie71 <johnnie.agonz71@gmail.com>
2022-01-11 10:25:48 -0500

    stages of loading and showing register or login buttons

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { Box, Flex, Link } from "@chakra-ui/react";
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
 import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
-	return (
-		<Flex bg="tomato" p={4} ml={"auto"}>
-			<Box ml={"auto"}>
+	const [{ data, fetching }] = useMeQuery();
+	let body = null;
+
+	// data is loading
+	if (fetching) {
+		body = null;
+	} else if (!data?.me) {
+		body = (
+			<>
 				<NextLink href={"/login"}>
 					<Link colorScheme="white" mr={2}>
 						Login
@@ -16,7 +23,22 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
 				<NextLink href={"/register"}>
 					<Link colorScheme="white">Register</Link>
 				</NextLink>
-			</Box>
+			</>
+		);
+		// user not logged in
+	} else {
+		body = (
+			<Flex>
+				<Box mr={2}>{data.me.username}</Box>
+				<Button variant="link">Logout</Button>
+			</Flex>
+		);
+		// user is logged in
+	}
+
+	return (
+		<Flex bg="tomato" p={4} ml={"auto"}>
+			<Box ml={"auto"}>{body}</Box>
 		</Flex>
 	);
 };
