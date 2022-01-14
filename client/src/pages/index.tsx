b043151a5008edb4a0f6ed3321930aeb6cf4d01a
johnnie71 <johnnie.agonz71@gmail.com>
2022-01-14 17:08:18 -0500

    removed urqlClient and wrapped it around the app component to make it accessible to the whole application

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,6 +1,4 @@
 import { NavBar } from "../components/NavBar";
-import { withUrqlClient } from "next-urql";
-import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
 
 const Index = () => {
@@ -18,4 +16,4 @@ const Index = () => {
 	);
 };
 
-export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
+export default Index;
