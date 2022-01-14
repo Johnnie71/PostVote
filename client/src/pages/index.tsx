11e5b77ceffbaf2f7c1aa1ddeda621f53e14871c
johnnie71 <johnnie.agonz71@gmail.com>
2022-01-14 16:14:48 -0500

    set server side rndering

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,11 +2,13 @@ import { NavBar } from "../components/NavBar";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 
-const Index = () => (
-	<div>
-		<NavBar />
-		<div>Hello World!</div>
-	</div>
-);
+const Index = () => {
+	return (
+		<div>
+			<NavBar />
+			<div>Hello World!</div>
+		</div>
+	);
+};
 
 export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
