0790c905c9e5612019b4ed660c36e0ff90fba9cc
johnnie71 <johnnie.agonz71@gmail.com>
2022-01-14 17:11:26 -0500

    slow delay to get posts

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -2,10 +2,13 @@ import { Post } from "../entities/Post";
 import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
 import { MyContext } from "./types";
 
+const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
 @Resolver()
 export class PostResolver {
 	@Query(() => [Post])
-	posts(@Ctx() { em }: MyContext): Promise<Post[]> {
+	async posts(@Ctx() { em }: MyContext): Promise<Post[]> {
+		await sleep(3000); // adding a slow delay to get posts
 		return em.find(Post, {});
 	}
 
