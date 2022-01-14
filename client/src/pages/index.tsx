6220c8a374e05a7fd716b61348120bcc4ccd8244
johnnie71 <johnnie.agonz71@gmail.com>
2022-01-14 16:18:42 -0500

    using PostQueryHook

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { NavBar } from "../components/NavBar";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
+import { usePostsQuery } from "../generated/graphql";
 
 const Index = () => {
+	const [] = usePostsQuery();
 	return (
 		<div>
 			<NavBar />
