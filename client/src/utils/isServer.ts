c9d8705e382bf609b24f2b0dbad7b81b4e958b83
johnnie71 <johnnie.agonz71@gmail.com>
2022-01-16 16:05:23 -0500

    comments

--- client/src/utils/isServer.ts
+++ client/src/utils/isServer.ts
@@ -1,2 +1,4 @@
 // checking to see if window variable is defined to see if it's running from server
 export const isServer = () => typeof window === "undefined";
+
+// if undefined it means we are on the server!
