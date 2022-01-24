5d0d4677c8b3df61bf9c070ad0735b9f4381645e
johnnie71 <johnnie.agonz71@gmail.com>
2022-01-24 14:57:47 -0500

    set up forgot password form

--- client/src/pages/forgot-password.tsx
+++ client/src/pages/forgot-password.tsx
@@ -1,49 +1,49 @@
-import { Box, Flex, Button, Link } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 import { Formik, Form } from "formik";
 import { withUrqlClient } from "next-urql";
-import NextLink from "next/link";
 import router from "next/router";
-import React from "react";
+import React, { useState } from "react";
 import { InputField } from "../components/InputField";
 import { Wrapper } from "../components/Wrapper";
+import { useForgotPasswordMutation } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { toErrorMap } from "../utils/toErrorMap";
 import login from "./login";
 
 const ForgotPassword: React.FC<{}> = ({}) => {
+	const [complete, setComplete] = useState(false);
+	const [, forgotPassword] = useForgotPasswordMutation();
 	return (
 		<Wrapper variant="small">
 			<Formik
-				initialValues={{ usernameOrEmail: "", password: "" }}
-				onSubmit={async (values, { setErrors }) => {
-					console.log(values);
-					const response = await login(values);
-					if (response.data?.login.errors) {
-						setErrors(toErrorMap(response.data.login.errors));
-					} else if (response.data?.login.user) {
-						// worked
-						router.push("/");
-					}
+				initialValues={{ email: "" }}
+				onSubmit={async (values) => {
+					await forgotPassword(values);
+					setComplete(true);
 				}}
 			>
-				{({ isSubmitting }) => (
-					<Form>
-						<InputField
-							name="email"
-							placeholder="email"
-							label="Email"
-							type="email"
-						/>
-						<Button
-							mt={4}
-							type="submit"
-							isLoading={isSubmitting}
-							colorScheme="teal"
-						>
-							Forgot Password
-						</Button>
-					</Form>
-				)}
+				{({ isSubmitting }) =>
+					complete ? (
+						<Box>If an account with that email exists, check your inbox</Box>
+					) : (
+						<Form>
+							<InputField
+								name="email"
+								placeholder="email"
+								label="Email"
+								type="email"
+							/>
+							<Button
+								mt={4}
+								type="submit"
+								isLoading={isSubmitting}
+								colorScheme="teal"
+							>
+								Forgot Password
+							</Button>
+						</Form>
+					)
+				}
 			</Formik>
 		</Wrapper>
 	);
