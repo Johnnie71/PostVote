0f121fe725f70e06ab417ccc3dbe14023e9f7151
johnnie71 <johnnie.agonz71@gmail.com>
2022-01-24 14:36:37 -0500

    forgot password form

--- client/src/pages/forgot-password.tsx
+++ client/src/pages/forgot-password.tsx
@@ -1,7 +1,52 @@
+import { Box, Flex, Button, Link } from "@chakra-ui/react";
+import { Formik, Form } from "formik";
+import { withUrqlClient } from "next-urql";
+import NextLink from "next/link";
+import router from "next/router";
 import React from "react";
+import { InputField } from "../components/InputField";
+import { Wrapper } from "../components/Wrapper";
+import { createUrqlClient } from "../utils/createUrqlClient";
+import { toErrorMap } from "../utils/toErrorMap";
+import login from "./login";
 
 const ForgotPassword: React.FC<{}> = ({}) => {
-	return <div>forgot password page</div>;
+	return (
+		<Wrapper variant="small">
+			<Formik
+				initialValues={{ usernameOrEmail: "", password: "" }}
+				onSubmit={async (values, { setErrors }) => {
+					console.log(values);
+					const response = await login(values);
+					if (response.data?.login.errors) {
+						setErrors(toErrorMap(response.data.login.errors));
+					} else if (response.data?.login.user) {
+						// worked
+						router.push("/");
+					}
+				}}
+			>
+				{({ isSubmitting }) => (
+					<Form>
+						<InputField
+							name="email"
+							placeholder="email"
+							label="Email"
+							type="email"
+						/>
+						<Button
+							mt={4}
+							type="submit"
+							isLoading={isSubmitting}
+							colorScheme="teal"
+						>
+							Forgot Password
+						</Button>
+					</Form>
+				)}
+			</Formik>
+		</Wrapper>
+	);
 };
 
-export default ForgotPassword;
+export default withUrqlClient(createUrqlClient)(ForgotPassword);
