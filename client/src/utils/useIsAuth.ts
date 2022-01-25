c32a91b6a92885db546938ef7a5e54e6ae2f3a83
johnnie71 <johnnie.agonz71@gmail.com>
2022-01-25 18:05:03 -0500

    redirecting to login then after login to createPost page

--- client/src/utils/useIsAuth.ts
+++ client/src/utils/useIsAuth.ts
@@ -5,11 +5,10 @@ import { useMeQuery } from "../generated/graphql";
 const useIsAuth = () => {
 	const [{ data, fetching }] = useMeQuery(); // to check whether they are logged in or not
 	const router = useRouter();
-
 	useEffect(() => {
 		if (!fetching && !data?.me) {
-			// if it's not loading and there is no user
-			router.replace("/login");
+			// if it's not loading and there is no user and push to login then after login send to create post page
+			router.replace(`/login?next=${router.pathname}`);
 		}
 	}, [fetching, data, router]);
 };
