7eeb116f31c004e4fa66e0ce88e5501234f0ee37
johnnie71 <johnnie.agonz71@gmail.com>
2022-01-25 17:30:33 -0500

    created a link to create post page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,13 +1,17 @@
+import { createUrqlClient } from "../utils/createUrqlClient";
+import { usePostsQuery } from "../generated/graphql";
 import { withUrqlClient } from "next-urql";
 import Layout from "../components/Layout";
-import { usePostsQuery } from "../generated/graphql";
-import { createUrqlClient } from "../utils/createUrqlClient";
+import { Link } from "@chakra-ui/react";
+import NextLink from "next/link";
 
 const Index = () => {
 	const [{ data }] = usePostsQuery();
 	return (
 		<Layout>
-			<div>Hello World!</div>
+			<NextLink href="create-post">
+				<Link>create post</Link>
+			</NextLink>
 			<br />
 			{data ? (
 				data.posts.map((post) => <div key={post.id}>{post.title}</div>)
