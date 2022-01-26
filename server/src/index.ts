5543043caffc56d060f2e1286e77a97d9db74be4
johnnie71 <johnnie.agonz71@gmail.com>
2022-01-26 17:42:50 -0500

    mock data

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,6 +14,7 @@ import Redis from "ioredis";
 import { createConnection } from "typeorm";
 import { Post } from "./entities/Post";
 import { User } from "./entities/User";
+import path from "path";
 
 const main = async () => {
 	const conn = await createConnection({
@@ -21,8 +22,10 @@ const main = async () => {
 		database: "lireddit2",
 		logging: true,
 		synchronize: true,
+		migrations: [path.join(__dirname, "./migrations/*")],
 		entities: [Post, User],
 	});
+	await conn.runMigrations(); // going to run migrations that not have been run
 
 	const app = express();
 
