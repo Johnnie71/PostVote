01400db66524c5987adfc0c9e22bb36a4fb01e17
johnnie71 <johnnie.agonz71@gmail.com>
2022-01-27 17:40:36 -0500

    ability to see user own email id they are the current user

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -3,10 +3,12 @@ import {
 	Arg,
 	Ctx,
 	Field,
+	FieldResolver,
 	Mutation,
 	ObjectType,
 	Query,
 	Resolver,
+	Root,
 } from "type-graphql";
 import { MyContext } from "./types";
 import argon2 from "argon2";
@@ -34,8 +36,19 @@ class UserResponse {
 	user?: User;
 }
 
-@Resolver()
+@Resolver(User)
 export class UserResolver {
+	@FieldResolver(() => String)
+	email(@Root() user: User, @Ctx() { req }: MyContext) {
+		// this is the current user and it's ok to show them their email
+		if (req.session.userId === user.id) {
+			return user.email;
+		}
+
+		// if current user wants to see someone elses email
+		return "";
+	}
+
 	@Mutation(() => UserResponse)
 	async changePassword(
 		@Arg("token") token: string,
