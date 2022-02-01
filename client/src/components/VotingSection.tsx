f30d2f8febb073315bfb586941530718a0366f1d
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-01 18:15:04 -0500

    handling upvote and down vote

--- client/src/components/VotingSection.tsx
+++ client/src/components/VotingSection.tsx
@@ -1,18 +1,37 @@
 import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
 import { IconButton, Flex } from "@chakra-ui/react";
 import React from "react";
-import { VoteSnippetFragment } from "../generated/graphql";
+import { useVoteMutation, VoteSnippetFragment } from "../generated/graphql";
 
 interface UpVoteSectionProps {
 	post: VoteSnippetFragment;
 }
 
 const VotingSection: React.FC<UpVoteSectionProps> = ({ post }) => {
+	const [, vote] = useVoteMutation();
 	return (
 		<Flex direction="column" justifyContent="center" alignItems="center" mr={4}>
-			<IconButton aria-label="up vote" icon={<ChevronDownIcon />} />
+			<IconButton
+				onClick={() => {
+					vote({
+						postId: post.id,
+						value: 1,
+					});
+				}}
+				aria-label="up vote"
+				icon={<ChevronDownIcon />}
+			/>
 			{post.points}
-			<IconButton aria-label="down vote" icon={<ChevronUpIcon />} />
+			<IconButton
+				onClick={() => {
+					vote({
+						postId: post.id,
+						value: -1,
+					});
+				}}
+				aria-label="down vote"
+				icon={<ChevronUpIcon />}
+			/>
 		</Flex>
 	);
 };
