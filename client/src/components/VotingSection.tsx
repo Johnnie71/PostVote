d9c36a37fa9ee7dea8bb1bf8a9abe0150555cf72
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-01 18:35:24 -0500

    isLoading functionality

--- client/src/components/VotingSection.tsx
+++ client/src/components/VotingSection.tsx
@@ -1,6 +1,6 @@
 import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
 import { IconButton, Flex } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import { useVoteMutation, VoteSnippetFragment } from "../generated/graphql";
 
 interface UpVoteSectionProps {
@@ -8,27 +8,37 @@ interface UpVoteSectionProps {
 }
 
 const VotingSection: React.FC<UpVoteSectionProps> = ({ post }) => {
+	const [loadingState, setLoadingState] = useState<
+		"upvote-loading" | "downvote-loading" | "not-loading"
+	>("not-loading");
 	const [, vote] = useVoteMutation();
+
 	return (
 		<Flex direction="column" justifyContent="center" alignItems="center" mr={4}>
 			<IconButton
-				onClick={() => {
-					vote({
+				onClick={async () => {
+					setLoadingState("upvote-loading");
+					await vote({
 						postId: post.id,
 						value: 1,
 					});
+					setLoadingState("not-loading");
 				}}
+				isLoading={loadingState === "upvote-loading"}
 				aria-label="up vote"
 				icon={<ChevronDownIcon />}
 			/>
 			{post.points}
 			<IconButton
-				onClick={() => {
-					vote({
+				onClick={async () => {
+					setLoadingState("downvote-loading");
+					await vote({
 						postId: post.id,
 						value: -1,
 					});
+					setLoadingState("not-loading");
 				}}
+				isLoading={loadingState === "downvote-loading"}
 				aria-label="down vote"
 				icon={<ChevronUpIcon />}
 			/>
