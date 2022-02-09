2a55434ede0b1279c9d92f852c820da915c26dcc
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-09 14:59:15 -0500

    adding router from next

--- client/src/pages/post/[id].tsx
+++ client/src/pages/post/[id].tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../../utils/createUrqlClient";
+import { useRouter } from "next/router";
 
 const Post = () => {
+	const router = useRouter();
 	return <div></div>;
 };
 
