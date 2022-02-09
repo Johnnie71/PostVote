19a51c5dc9b978519a5bb638d7e7884868ff3143
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-09 16:07:06 -0500

    centered navbar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -34,9 +34,11 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
 	} else {
 		// user is logged in
 		body = (
-			<Flex>
+			<Flex align="center">
 				<NextLink href="create-post">
-					<Link mr={2}>create post</Link>
+					<Button colorScheme="green" as={Link} mr={4}>
+						create post
+					</Button>
 				</NextLink>
 				<Box mr={2}>{data.me.username}</Box>
 				<Button
@@ -53,21 +55,15 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
 	}
 
 	return (
-		<Flex
-			zIndex={1}
-			position="sticky"
-			top={0}
-			bg="tan"
-			p={4}
-			ml={"auto"}
-			align="center"
-		>
-			<NextLink href="/">
-				<Link>
-					<Heading>LiReddit</Heading>
-				</Link>
-			</NextLink>
-			<Box ml={"auto"}>{body}</Box>
+		<Flex zIndex={1} position="sticky" top={0} bg="tan" p={4}>
+			<Flex flex={1} m="auto" align="center" maxW={800}>
+				<NextLink href="/">
+					<Link>
+						<Heading>LiReddit</Heading>
+					</Link>
+				</NextLink>
+				<Box ml={"auto"}>{body}</Box>
+			</Flex>
 		</Flex>
 	);
 };
