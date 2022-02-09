1f0e1ac2cc322828732e2c7f25f9975c53c0ae32
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-09 15:33:34 -0500

    getting data for the post using id

--- client/src/pages/post/[id].tsx
+++ client/src/pages/post/[id].tsx
@@ -2,10 +2,33 @@ import React from "react";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../../utils/createUrqlClient";
 import { useRouter } from "next/router";
+import { usePostQuery } from "../../generated/graphql";
+import Layout from "../../components/Layout";
 
 const Post = () => {
 	const router = useRouter();
-	return <div></div>;
+	const intId =
+		typeof router.query.id === "string" ? parseInt(router.query.id) : -1;
+	const [{ data, error, fetching }] = usePostQuery({
+		pause: intId === -1,
+		variables: {
+			id: intId,
+		},
+	});
+
+	if (fetching) {
+		return (
+			<Layout>
+				<div>loading...</div>
+			</Layout>
+		);
+	}
+
+	if (error) {
+		return <div>{error.message}</div>;
+	}
+
+	return <Layout>{data?.post?.text}</Layout>;
 };
 
 export default withUrqlClient(createUrqlClient, { ssr: true })(Post);
