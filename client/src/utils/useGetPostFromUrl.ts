0c263cafd6b64e81f46e61f9b16a40466d3c3d61
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-10 11:46:43 -0500

    small editing

--- client/src/utils/useGetPostFromUrl.ts
+++ client/src/utils/useGetPostFromUrl.ts
@@ -5,6 +5,7 @@ export const useGetPostFromUrl = () => {
 	const router = useRouter();
 	const intId =
 		typeof router.query.id === "string" ? parseInt(router.query.id) : -1;
+
 	return usePostQuery({
 		pause: intId === -1,
 		variables: {
