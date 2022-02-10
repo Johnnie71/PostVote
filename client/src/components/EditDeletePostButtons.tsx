c95a1320accc4493c93f14c58b06ec0f276db085
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-10 12:20:08 -0500

    using me query to render buttons

--- client/src/components/EditDeletePostButtons.tsx
+++ client/src/components/EditDeletePostButtons.tsx
@@ -2,16 +2,23 @@ import { EditIcon, DeleteIcon } from "@chakra-ui/icons";
 import { Box, IconButton, Link } from "@chakra-ui/react";
 import NextLink from "next/link";
 import React from "react";
-import { useDeletePostMutation } from "../generated/graphql";
+import { useDeletePostMutation, useMeQuery } from "../generated/graphql";
 
 interface EditDeletePostButtonsProps {
 	id: number;
+	creatorId: number;
 }
 
 const EditDeletePostButtons: React.FC<EditDeletePostButtonsProps> = ({
 	id,
+	creatorId,
 }) => {
+	const [{ data: meData }] = useMeQuery();
 	const [, deletePost] = useDeletePostMutation();
+
+	if (meData?.me?.id !== creatorId) {
+		return null;
+	}
 	return (
 		<Box>
 			<NextLink href="/post/edit/[id]" as={`/post/edit/${id}`}>
