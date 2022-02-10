ee248844051ce5faa0bc167801c8591e0fe5fd95
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-10 11:44:08 -0500

    filling in form with post title and text

--- client/src/pages/post/edit/[id].tsx
+++ client/src/pages/post/edit/[id].tsx
@@ -3,16 +3,34 @@ import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../../../utils/createUrqlClient";
 import { Box, Button } from "@chakra-ui/react";
 import { Formik, Form } from "formik";
-import router from "next/router";
 import { InputField } from "../../../components/InputField";
 import Layout from "../../../components/Layout";
 import createPost from "../../create-post";
+import { useGetPostFromUrl } from "../../../utils/useGetPostFromUrl";
 
 const EditPost = () => {
+	const [{ data, fetching }] = useGetPostFromUrl();
+
+	if (fetching) {
+		return (
+			<Layout>
+				<div>loading.....</div>
+			</Layout>
+		);
+	}
+
+	if (!data?.post) {
+		return (
+			<Layout>
+				<div>could not find post</div>
+			</Layout>
+		);
+	}
+
 	return (
 		<Layout variant="small">
 			<Formik
-				initialValues={{ title: "", text: "" }}
+				initialValues={{ title: data.post.title, text: data.post.text }}
 				onSubmit={async (values) => {
 					// const { error } = await createPost({ input: values });
 					// if (!error) {
