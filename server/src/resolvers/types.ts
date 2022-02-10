d51c5518214e939f163385d2e6a64b028431f00d
johnnie71 <johnnie.agonz71@gmail.com>
2022-02-10 13:06:12 -0500

    added userLoader

--- server/src/resolvers/types.ts
+++ server/src/resolvers/types.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
 import { Redis } from "ioredis";
+import { createUserLoader } from "src/utils/createUserLoader";
 
 export type MyContext = {
 	req: Request & { session: { userId?: number } };
 	redis: Redis;
 	res: Response;
+	userLoader: ReturnType<typeof createUserLoader>;
 };
